e0f6428b15b4d59221a01b26cdfd0d507242a3fa
MANMINAMERICA <iglesiamanmincolombia@gmail.com>
2026-09-11 11:10:29 -0500

    Add files via upload

---
+++ biblia-deploy/sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'biblia-bilingue-v7';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/js/app.js',
+    '/manifest.json',
+    '/icons/icon-192.png',
+    '/icons/icon-512.png',
+    '/data/bible-combined.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(ASSETS);
+        }).then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(cached => {
+            return cached || fetch(event.request).then(response => {
+                if (response.status === 200) {
+                    const clone = response.clone();
+                    caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+                }
+                return response;
+            });
+        }).catch(() => {
+            if (event.request.destination === 'document') {
+                return caches.match('/index.html');
+            }
+        })
+    );
+});
